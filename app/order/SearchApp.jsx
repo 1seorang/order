6b9849c8991f337a0fc2651ec8ef96b7d03054db
1seorang <aby.packaging@gmail.com>
2024-02-16 16:33:09 +0700

    update

--- app/order/SearchApp.jsx
+++ app/order/SearchApp.jsx
@@ -27,6 +27,21 @@ function SearchApp() {
   const [searchTerm, setSearchTerm] = useState("");
   const [submittedSearchTerm, setSubmittedSearchTerm] = useState("");
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    if (loading) {
+      // Fetch data from your API
+      fetch(
+        "https://sheets.googleapis.com/v4/spreadsheets/1ZJKK2v-8r8FJtMx9hTiYI8YJrjqW0C6KR4gF964UaTs/values/PACK?alt=json&key=AIzaSyBgoFd8Gi1YDUSc6llH9TxMDSpkr7jcavU"
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          setData(data.values);
+          setFilteredData(data);
+          setLoading(false);
+        });
+    }
+  }, [loading]);
   useEffect(() => {
     // Fetch data from your API
     fetch(
@@ -145,173 +160,187 @@ function SearchApp() {
               tabContent: "group-data-[selected=true]:text-[#ddf]",
             }}
           >
-            {submittedSearchTerm && filteredData
-              ? filteredData.map((item, index) => (
-                <Tab key={index} title={item[0]}>
-                  <Card className="w-full py-1 print:bg-red-950" id="card1">
-                    <CardHeader className="block gap-1 p-1">
-                      <h4 className="text-medium font-medium drop-shadow-xl shadow-gray-200">
-                        {" "}
-                        {item[1]}{" "}
-                      </h4>
-                      <p className=" text-default-500">
-                        {" "}
-                        Status: {item[16]}{" "}
-                      </p>
-                      <Button
-                        onPress={onOpen}
-                        className="absolute top-0 right-0 p-0  m-0 hover:opacity-80 opacity-50 transition-all hover:bg-blue-700 bg-blue-500/60"
-                        size="sm"
-                        title="Lihat Detail"
-                      >
-                        <FaArrowUpRightFromSquare className="" />
-                      </Button>
-                    </CardHeader>
-                    <Divider />
-                    <CardBody className="py-1 px-2">
-                      <div>
-                        {" "}
-                        <Modal
-                          size="md"
-                          backdrop="blur"
-                          isOpen={isOpen}
-                          isDismissable={false}
-                          placement="center"
-                          onOpenChange={onOpenChange}
-                          radius="3xl"
-                          classNames={{
-                            body: "py-3",
-                            // backdrop: "bg-[#292f46]/70 backdrop-opacity-60",
-                            base: "border-[#292f46] bg-[#19172c] dark:bg-[#19172c] text-[#a8b0d3]",
-                            header: "border-b-[1px] border-[#292f46]",
-                            footer: "border-t-[1px] border-[#292f46]",
-                            closeButton:
-                              "hover:bg-white/5 text-red-500/70 hover:text-red-500/90 active:bg-white/10 z-10",
-                          }}
+            {loading ? (
+              <Tab key="load1" title=" ">
+                <Divider />
+                <Spacer className="my-3" />
+                <Spinner
+                  label="Loading...!"
+                  color="secondary"
+                  labelColor="secondary"
+                  size="xl"
+                />
+              </Tab>
+            ) : // "loading"
+              !filteredData ? (
+                "loading"
+              ) : filteredData.length > 0
+                ? filteredData.map((item, index) => (
+                  <Tab key={index} title={item[0]}>
+                    <Card className="w-full py-1 print:bg-red-950" id="card1">
+                      <CardHeader className="block gap-1 p-1">
+                        <h4 className="text-medium font-medium drop-shadow-xl shadow-gray-200">
+                          {" "}
+                          {item[1]}{" "}
+                        </h4>
+                        <p className=" text-default-500">
+                          {" "}
+                          Status: {item[16]}{" "}
+                        </p>
+                        <Button
+                          onPress={onOpen}
+                          className="absolute top-0 right-0 p-0  m-0 hover:opacity-80 opacity-50 transition-all hover:bg-blue-700 bg-blue-500/60"
+                          size="sm"
+                          title="Lihat Detail"
                         >
-                          <ModalContent>
-                            {(onClose) => (
-                              <>
-                                <Watermark
-                                  content="Aby"
-                                  rotate={-45}
-                                  fontFamily="FontSans"
-                                  width={70}
-                                  height={15}
-                                  fontSize={22}
-                                  fontColor="rgb(255 0 0 / 25%)"
-                                >
+                          <FaArrowUpRightFromSquare className="" />
+                        </Button>
+                      </CardHeader>
+                      <Divider />
+                      <CardBody className="py-1 px-2">
+                        <div>
+                          {" "}
+                          <Modal
+                            size="md"
+                            backdrop="blur"
+                            isOpen={isOpen}
+                            isDismissable={false}
+                            placement="center"
+                            onOpenChange={onOpenChange}
+                            radius="3xl"
+                            classNames={{
+                              body: "py-3",
+                              // backdrop: "bg-[#292f46]/70 backdrop-opacity-60",
+                              base: "border-[#292f46] bg-[#19172c] dark:bg-[#19172c] text-[#a8b0d3]",
+                              header: "border-b-[1px] border-[#292f46]",
+                              footer: "border-t-[1px] border-[#292f46]",
+                              closeButton:
+                                "hover:bg-white/5 text-red-500/70 hover:text-red-500/90 active:bg-white/10 z-10",
+                            }}
+                          >
+                            <ModalContent>
+                              {(onClose) => (
+                                <>
                                   <Watermark
-                                    content="© 2023"
+                                    content="Aby"
                                     rotate={-45}
-                                    offsetTop={117}
-                                    offsetLeft={117}
+                                    fontFamily="FontSans"
                                     width={70}
                                     height={15}
-                                    fontSize={8}
+                                    fontSize={22}
                                     fontColor="rgb(255 0 0 / 25%)"
                                   >
-                                    <ModalHeader className="flex flex-col gap-1">
-                                      {item[2] || item[0]}
-                                    </ModalHeader>
-                                    <ModalBody className="text-xs uppercase">
-                                      <p>
-                                        Dept : - <br />
-                                        Date : {item[5]}
-                                      </p>
-                                      <p>
-                                        Mat. Code : {item[3]} <br />
-                                        Mat. Name : {item[4]} <br />
-                                        Qty : {item[7]} {item[8]}
-                                      </p>
-                                      <p>
-                                        No PR : {item[9]} <br />
-                                        No PO : {item[15]}
-                                      </p>
-                                      <p>
-                                        Remark : {item[13]}
-                                        <br />
-                                        user : {item[12]}
-                                      </p>
-                                      <p>
-                                        Vendor : {item[16]}
-                                        <br />
-                                        Tgl. GR : {item[17]}
-                                      </p>
-                                    </ModalBody>
-                                    <ModalFooter className="py-2">
-                                      <Button
-                                        color="danger"
-                                        variant="ghost"
-                                        onPress={onClose}
-                                      >
-                                        Close
-                                      </Button>
-                                      <Button
-                                        className="bg-[#6f4ef2] shadow-lg shadow-indigo-500/20 hover:backdrop-blur-lg hover:bg-blue-500/50 transition duration-500"
-                                        onPress={onClose}
-                                      >
-                                        Close (Juga)
-                                      </Button>
-                                    </ModalFooter>
+                                    <Watermark
+                                      content="© 2023"
+                                      rotate={-45}
+                                      offsetTop={117}
+                                      offsetLeft={117}
+                                      width={70}
+                                      height={15}
+                                      fontSize={8}
+                                      fontColor="rgb(255 0 0 / 25%)"
+                                    >
+                                      <ModalHeader className="flex flex-col gap-1">
+                                        {item[2] || item[0]}
+                                      </ModalHeader>
+                                      <ModalBody className="text-xs uppercase">
+                                        <p>
+                                          Dept : {item[18]} <br />
+                                          Date : {item[5]}
+                                        </p>
+                                        <p>
+                                          Mat. Code : {item[3]} <br />
+                                          Mat. Name : {item[4]} <br />
+                                          Qty : {item[7]} {item[8]}
+                                        </p>
+                                        <p>
+                                          No PR : {item[9]} <br />
+                                          No PO : {item[15]}
+                                        </p>
+                                        <p>
+                                          Remark : {item[13]}
+                                          <br />
+                                          user : {item[12]}
+                                        </p>
+                                        <p>
+                                          Vendor : {item[16]}
+                                          <br />
+                                          Tgl. GR : {item[17]}
+                                        </p>
+                                      </ModalBody>
+                                      <ModalFooter className="py-2">
+                                        <Button
+                                          color="danger"
+                                          variant="ghost"
+                                          onPress={onClose}
+                                        >
+                                          Close
+                                        </Button>
+                                        <Button
+                                          className="bg-[#6f4ef2] shadow-lg shadow-indigo-500/20 hover:backdrop-blur-lg hover:bg-blue-500/50 transition duration-500"
+                                          onPress={onClose}
+                                        >
+                                          Close (Juga)
+                                        </Button>
+                                      </ModalFooter>
+                                    </Watermark>
                                   </Watermark>
-                                </Watermark>
-                              </>
-                            )}
-                          </ModalContent>
-                        </Modal>
-                      </div>
-                      <p className="text-default-800"> {item[4]} </p>
-                      <p className="text-small text-default-700">
-                        {" "}
-                        ({item[7] + item[8]})
-                      </p>
-                      <Spacer y={4} />
-                      <Code color="secondary">
-                        <p className="text-default-700">
-                          No. PR
-                          {": "}
-                          <Snippet size="sm" symbol=" ">
-                            {item[9]}
-                          </Snippet>
+                                </>
+                              )}
+                            </ModalContent>
+                          </Modal>
+                        </div>
+                        <p className="text-default-800"> {item[4]} </p>
+                        <p className="text-small text-default-700">
+                          {" "}
+                          ({item[7] + item[8]})
                         </p>
-                      </Code>
-                      <Spacer y={2} />
-                      <Code>
-                        <p className="text-small text-default-700 w-full">
-                          No. PO{": "}
-                          <Snippet
-                            variant="solid"
-                            color="secondary"
-                            size="sm"
-                            symbol=" "
-                          >
-                            {item[17]}
-                          </Snippet>
-                        </p>
-                      </Code>
-                    </CardBody>
-                    <Divider />
-                    <CardFooter>
-                      <Code>
-                        <p className="text-small text-default-700 w-full">
-                          Vendor: {item[18]}
-                        </p>
-                      </Code>
-                      <Button
-                        className="absolute right-0 bottom-0 hover:bg-purple-500/40 bg-purple-300/40 transition-all"
-                        title="Print"
-                        size="sm"
-                        onClick={Print}
-                      >
-                        <FaFileDownload className="p-0 m-0 text-lg fill-slate-200 stroke-yellow-950" />
-                      </Button>
-                    </CardFooter>
-                  </Card>
-                </Tab>
-                // <li key={index}>{JSON.stringify(item)}</li>
-              ))
-              : null}
+                        <Spacer y={4} />
+                        <Code color="secondary">
+                          <p className="text-default-700">
+                            No. PR
+                            {": "}
+                            <Snippet size="sm" symbol=" ">
+                              {item[9]}
+                            </Snippet>
+                          </p>
+                        </Code>
+                        <Spacer y={2} />
+                        <Code>
+                          <p className="text-small text-default-700 w-full">
+                            No. PO{": "}
+                            <Snippet
+                              variant="solid"
+                              color="secondary"
+                              size="sm"
+                              symbol=" "
+                            >
+                              {item[15]}
+                            </Snippet>
+                          </p>
+                        </Code>
+                      </CardBody>
+                      <Divider />
+                      <CardFooter>
+                        <Code>
+                          <p className="text-small text-default-700 w-full">
+                            Vendor: {item[16]}
+                          </p>
+                        </Code>
+                        <Button
+                          className="absolute right-0 bottom-0 hover:bg-purple-500/40 bg-purple-300/40 transition-all"
+                          title="Print"
+                          size="sm"
+                          onClick={Print}
+                        >
+                          <FaFileDownload className="p-0 m-0 text-lg fill-slate-200 stroke-yellow-950" />
+                        </Button>
+                      </CardFooter>
+                    </Card>
+                  </Tab>
+                  // <li key={index}>{JSON.stringify(item)}</li>
+                ))
+                : null}
           </Tabs>
         </div>
       </div>
---
+++ components/BottomNav.jsx
@@ -0,0 +1,97 @@
+import React from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  NavbarMenuToggle,
+  NavbarMenu,
+  NavbarMenuItem,
+} from "@nextui-org/navbar";
+import {
+  IconSearchengin,
+  IconHome2,
+  IconMessageText,
+  IconBxsCalculator,
+  IconBxEdit,
+} from "./icons";
+import { Button } from "@nextui-org/button";
+import NextLink from "next/link";
+
+function BottomNav() {
+  return (
+    <div className="fixed bottom-0 flex al sm:hidden w-full z-50">
+      <Navbar className="flex justify-center w-full z-49">
+        <NavbarContent className="w-full flex justify-center text-center  h-full z-48 ">
+          <NavbarItem className="w-full flex justify-center">
+            <NextLink href="/" color="primary" variant="light">
+              <IconHome2 className="stroke-cyan-500 hover:stroke-cyan-700 text-3xl" />
+              <small className="text-cyan-500">Home</small>
+            </NextLink>
+          </NavbarItem>
+        </NavbarContent>
+
+        <NavbarContent className="w-full flex justify-center text-center  h-full z-48 ">
+          <NavbarItem className="w-full flex justify-center">
+            <NextLink href="/note" color="primary" variant="light">
+              <IconBxEdit className="stroke-cyan-500 hover:stroke-cyan-700 text-3xl" />
+              <small className="text-cyan-500">Note</small>
+            </NextLink>
+          </NavbarItem>
+        </NavbarContent>
+
+        <NavbarContent className="text-center">
+          <NavbarItem className="w-full flex justify-center">
+            <NextLink href="/loading">
+              <Button
+                isIconOnly
+                aria-label="cari"
+                className=""
+                variant="light"
+                color="primary"
+              >
+                <IconSearchengin className="stroke-cyan-500 hover:stroke-cyan-700 text-5xl" />
+                <small className="text-cyan-500"></small>
+              </Button>
+            </NextLink>
+          </NavbarItem>
+        </NavbarContent>
+
+        {/* <NavbarContent>
+					<NavbarItem className="w-full flex justify-center">
+						<NextLink href="/calculator"  color="primary"  variant="light">
+							<Button isIconOnly  aria-label="sos">
+								<IconBxsCalculator className="stroke-cyan-500 hover:stroke-cyan-700 text-3xl" />
+							<small className="text-cyan-500">Kalkulator</small>
+							</Button>
+						</NextLink>
+					</NavbarItem>
+				</NavbarContent> */}
+        <NavbarContent className="w-full flex justify-center text-center  h-full z-48 ">
+          <NavbarItem className="w-full flex justify-center">
+            <NextLink
+              href="/kalkulator"
+              color="primary"
+              className="justify-center  "
+              variant="light"
+            >
+              <IconBxsCalculator className="stroke-cyan-500 hover:stroke-cyan-700 text-3xl justify-self-center " />
+              <small className="text-cyan-500">Calc</small>
+            </NextLink>
+          </NavbarItem>
+        </NavbarContent>
+
+        <NavbarContent className="w-full flex justify-center text-center  h-full z-48 ">
+          <NavbarItem className="w-full flex justify-center">
+            <NextLink href="/loading" color="primary" variant="light">
+              <IconMessageText className="stroke-cyan-500 hover:stroke-cyan-700 text-3xl " />
+              <small className="text-cyan-500">Pesan</small>
+            </NextLink>
+          </NavbarItem>
+        </NavbarContent>
+      </Navbar>
+    </div>
+  );
+}
+
+export default BottomNav;
